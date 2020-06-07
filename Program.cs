7bb865469df9dc2dbc866be39f5330f8df30f5a8
Nathan Button <nathanebutton@gmail.com>
2020-06-07 03:47:06 +0000

    Add board input state collection

--- Program.cs
+++ Program.cs
@@ -1,12 +1,55 @@
 ﻿using System;
+using static eyeScrabble.ConsoleUtils;
 
 namespace eyeScrabble
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        private static char[,] board = new char[7, 7];
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ReadBoard();
+            for (int x = 0; x < 7; x++)
+            {
+                for (int y = 0; y < 7; y++)
+                {
+                }
+            }
+        }
+
+        private static void ReadBoard()
+        {
+            for (int x = 0; x < 7; x++)
+            {
+                char[] curLine;
+                while (true)
+                {
+                    Print($"Line #{x}: ");
+                    curLine = Console.ReadLine().ToCharArray();
+                    if (curLine.Length == 7)
+                    {
+                        break;
+                    }
+                }
+
+                for (int i = 0; i < curLine.Length; i++)
+                {
+                    board[x, i] = curLine[i];
+                }
+
+            }
         }
     }
+    public static class ConsoleUtils
+    {
+        public static void PrintL(object obj)
+        {
+            Console.WriteLine(obj);
+        }
+        public static void Print(object obj)
+        {
+            Console.Write(obj);
+        }
+    }
+
 }
